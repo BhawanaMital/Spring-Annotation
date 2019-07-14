28289d2cb81a999491d47d453fbb15f731f44372
bhawana <mital.bhawana@gmail.com>
2019-07-14 16:39:09 +0530

    Task 2

--- src/main/java/com/stakeroute/Main.java
+++ src/main/java/com/stakeroute/Main.java
@@ -7,20 +7,18 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class Main {
     public static void main(String[] args) {
-//        AnnotationConfigApplicationContext context=new AnnotationConfigApplicationContext();
-//        context.register(Config.class);
-//        context.refresh();
-//
-//        Movie movie=context.getBean("movie",Movie.class);
-//        System.out.println(movie);
-
         AnnotationConfigApplicationContext context=new AnnotationConfigApplicationContext();
         context.register(Config.class);
         context.refresh();
-        Movie movie=context.getBean("movie", Movie.class);
+
+        Movie movie=context.getBean("movieA",Movie.class);
         System.out.println(movie);
 
 
- }
-}
+        Movie movie1=context.getBean("movieA", Movie.class);
+        Movie movie2=context.getBean("movieB", Movie.class);
 
+        System.out.println(movie1==movie2);
+    }
+
+}
--- src/main/java/com/stakeroute/domain/Config.java
+++ src/main/java/com/stakeroute/domain/Config.java
@@ -3,6 +3,7 @@ package com.stakeroute.domain;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class Config {
@@ -17,6 +18,32 @@ public class Config {
     }
 
     @Bean
+    public Actor actor1() {
+        Actor actor = new Actor("shibani","female" ,19);
+
+        return actor;
+
+    }
+
+    @Bean
+    public Actor actor2() {
+        Actor actor = new Actor("tushar", "male", 17);
+
+        return actor;
+    }
+
+    public Actor actor3() {
+        Actor actor = new Actor("yogi","male" ,15);
+
+        return actor;
+
+
+
+    }
+
+
+    @Bean({"movieA","movieB"})
+    @Scope("prototype")
     public Movie movie()
     {
         Movie movie = new Movie(actor());
